bb8296ef0abe7cbb7472332f46a234bea94dc51a
mac <yiguangzhou@163.com>
2016-06-07 23:10:03 +0800

    更新用户数据保存

--- app/src/main/java/com/example/mac/xmppdemo01/common/register.java
+++ app/src/main/java/com/example/mac/xmppdemo01/common/register.java
@@ -1,5 +1,6 @@
 package com.example.mac.xmppdemo01.common;
 
+
 import org.jivesoftware.smack.PacketCollector;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPConnection;
@@ -9,9 +10,9 @@ import org.jivesoftware.smack.filter.PacketIDFilter;
 import org.jivesoftware.smack.filter.PacketTypeFilter;
 import org.jivesoftware.smack.packet.IQ;
 import org.jivesoftware.smack.packet.Registration;
+/*
 
-/**
- * Created by mac on 16/6/7.
+//1 注册成功, 0,服务器没有返回结果, 2, 这个账号已经存在,3,注册失败
 public class register {
     public String register(XMPPConnection conn, String account, String password) throws SmackException.NotConnectedException {
         if (conn == null) {
@@ -21,14 +22,13 @@ public class register {
         reg.setType(IQ.Type.SET);
         reg.setTo(conn.getServiceName());
 
-        PacketFilter filter = new AndFilter(new PacketIDFilter(
-                reg.getPacketID()), new PacketTypeFilter(IQ.class)
-        );
-        PacketCollector collector = conn.createPacketCollector(filter);
-        conn.sendPacket(reg);
+       // reg.setUsername(account);
+       // reg.setPassword(password);
+
 
     }
 }
 
 }
- */
+
+*/
